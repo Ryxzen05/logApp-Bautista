cf15f3b31b7ee1149e9b05083f45f1e96a79c912
John Paul Bautista <201980161@psu.palawan.edu.ph>
2021-10-28 18:12:57 +0800

    First commit

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'https://github.com/Ryxzen05/logApp-Bautista.git');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6447370');
+	define('DB_PASS', 'eNdrXV39zP');
+	define('DB_NAME', 'sql6447370');
